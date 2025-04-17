96da941da831655861b48aada21a7e962120a9cd
vvendelsantos <producao.wendel@gmail.com>
2025-04-17 18:53:34 -0300

    corpus_app.py

--- corpus_app.py
+++ corpus_app.py
@@ -7,7 +7,7 @@ from word2number import w2n
 # Função para converter números por extenso para algarismos
 def converter_numeros_por_extenso(texto):
     unidades = {
-        "zero": 0, "um": 1, "uma": 1, "dois": 2, "duas": 2, "três": 3, "quatro": 4, "cinco": 5,
+        "zero": 0, "dois": 2, "duas": 2, "três": 3, "quatro": 4, "cinco": 5,
         "seis": 6, "sete": 7, "oito": 8, "nove": 9
     }
     dezenas = {
