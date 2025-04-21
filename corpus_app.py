4b90381e544fdf4cc451cd4ba1ffffe845ac5004
vvendelsantos <producao.wendel@gmail.com>
2025-04-21 17:04:20 -0300

    corpus_app.py

--- corpus_app.py
+++ corpus_app.py
@@ -6,7 +6,7 @@ import spacy
 from word2number import w2n
 
 # Carregar modelo do spaCy
-nlp = spacy.load("pt_core_news_sm")
+nlp = spacy.load("pt_core_news_lg")
 
 # Funções da parte 1
 def detectar_siglas(texto):
