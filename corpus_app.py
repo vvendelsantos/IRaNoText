9a348a152b8cda97a35a668e0131b4192580b71f
vvendelsantos <producao.wendel@gmail.com>
2025-04-25 09:53:39 -0300

    corpus_app.py

--- corpus_app.py
+++ corpus_app.py
@@ -140,7 +140,7 @@ with tabs[1]:
             return texto
             
         numeros = {
-            "zero": "0", "um": "1", "uma": "1", "dois": "2", "duas": "2", 
+            "zero": "0", "dois": "2", "duas": "2", 
             "três": "3", "quatro": "4", "cinco": "5", "seis": "6", 
             "sete": "7", "oito": "8", "nove": "9", "dez": "10",
             "onze": "11", "doze": "12", "treze": "13", "quatorze": "14", 
